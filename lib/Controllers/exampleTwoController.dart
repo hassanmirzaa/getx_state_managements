7106d4d2539766b696f6e9bb1c2c904f87bcd198
Hassan Mirza <141772015+hassanmirzaa@users.noreply.github.com>
2024-11-14 19:20:39 +0500

    .

--- lib/Controllers/exampleTwoController.dart
+++ lib/Controllers/exampleTwoController.dart
@@ -1,11 +1,9 @@
-
-
 import 'package:get/get.dart';
 
-class ExampleTwoController extends GetxController{
+class ExampleTwoController extends GetxController {
   RxDouble opacity = 0.4.obs;
 
-  setOpacity(double value){
+  void setOpacity(double value) {
     opacity.value = value;
   }
 }
